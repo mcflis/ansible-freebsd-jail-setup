a6ee5ff6c3cd9b4abd22c5fdb177023fa12d6442
Maximilian Flis <19263910+mcflis@users.noreply.github.com>
2021-03-07 18:44:56 +0100

    Feature/initial iteration (#1)
    * chore: update README.md
    
    * chore: ignore idea folder
    
    * chore: add .editorconfig
    
    * feat: ensure the package management tool is installed
    
    * feat: add example.conf.sh and ignore the actual conf file
    
    * feat: move pkg install to readme as it cannot be part of the script, use conf file, install packages
    
    * feat: add -y option when installing git, do not link python, install py37 instead of 39
    
    * feat: add further install steps
    
    * feat: add ansible playbook
    
    * feat: add ansible config+inventory to avoid specifying cli opts
    
    * feat: use service module to start and enable sshd
    
    * feat: remove inventory because it is not needed for implicit localhost
    
    * feat: adding ssh user
    
    * feat: ensure borg user shell and groups are as required
    
    * feat: ensure borg user can sudo
    
    * feat: add default vars
    
    * feat: include vars and not tasks
    
    * feat: rename example to default.conf.sh
    
    * feat: add another how to step to readme
    
    * chore: move editorconfig to toplevel
    
    * refactor: drop complex step-by-step installation and replace with all-in-one install script
    
    * refactor: add another vars path because archive is extracted without retaining directory structure when using all-in-one install script
    
    * feat: remove temp dir
    
    * feat: add one-shot cmd to README.md
    
    * feat: ensure packages are installed

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ansible-borg-backup-jail-setup
-Utilities to setup a TrueNAS jail for running borg backup as client
+
+A repo for setting up a TrueNAS jail for running borg backup as client.
+
+The scripts and playbooks in this repository are meant to be executed in the freebsd jail.
+
+## Install
+
+One-shot command to install curl and (because it is not provided by default) and then run the `install.sh` script
+
+```sh
+sh -c "env ASSUME_ALWAYS_YES=YES pkg bootstrap && \
+       pkg update && \
+       pkg install -y curl && \
+       curl -Lo- https://raw.githubusercontent.com/mcflis/ansible-borg-backup-jail-setup/feature/initial-iteration/install.sh | sh"
+```
+
+## Resources:
+
+- [1] https://www.cyberciti.biz/faq/how-to-enable-sshd-on-freebsd-server-jail/
---
+++ install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+set -e
+
+VERSION=${VERSION-main}
+REPO_URL="https://github.com/mcflis/ansible-borg-backup-jail-setup/archive/$VERSION.zip"
+
+echo "[Stage] bootstrap"
+env ASSUME_ALWAYS_YES=YES pkg bootstrap
+echo ""
+
+echo ""
+echo "[Stage] ansible-dependencies"
+pkg update
+pkg install -y py37-ansible
+echo ""
+
+echo ""
+echo "[Stage] repository"
+echo "Creating temporary directory"
+prefix="ansible-setup-$(echo "$VERSION" | sed -e 's/[^A-Za-z0-9._-]/_/g')"
+tempdir=$(mktemp -dt "$prefix")
+archive="$tempdir/$prefix.zip"
+echo "Temporary directory: $tempdir"
+echo "Downloading repo from $REPO_URL"
+curl "$REPO_URL" -Lo "$archive"
+unzip "$archive" -jd "$tempdir"
+ANSIBLE_CFG="$tempdir/ansible.cfg"
+export ANSIBLE_CFG
+echo ""
+
+echo ""
+echo "[Stage] ansible-ping"
+ansible -m ping -c local localhost
+echo ""
+
+echo ""
+echo "[Stage] ansible-ping"
+ansible-playbook "$tempdir/main.yml"
+echo ""
+
+echo "[Stage] cleanup"
+rm -rf "$tempdir"
+echo ""
